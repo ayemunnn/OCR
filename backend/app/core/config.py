e52ee7a0479d7d3647cab09b79ce315609d30eee
ayemunnn <91075315+ayemunnn@users.noreply.github.com>
2026-05-22 19:48:35 -0400

    frontend link added in cors

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -24,6 +24,7 @@ class Settings(BaseSettings):
         "http://127.0.0.1:3000",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://ashy-tree-0793b110f.7.azurestaticapps.net",
     ]
 
     model_config = SettingsConfigDict(
